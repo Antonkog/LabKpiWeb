967da486271a0cfeeeabbbc24cb73811174897fc
Anton Kogan <a.kogan777@gmail.com>
2020-12-19 23:15:25 +0200

    change jobs position on "Experience" click.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 const toggle = function (elem) {
-  elem.classList.toggle('d-none');
+    elem.classList.toggle('d-none');
 };
 
+const swap = function(elem) {
+    var divs = elem.getElementsByTagName("div");
+    divs[0].parentNode.appendChild(divs[0].parentNode.replaceChild(divs[2], divs[0]));
+};
+
+
 document.addEventListener(
-  'click',
-  function (event) {
-    if (!event.target.classList.contains('toggle')) return;
-    event.preventDefault();
-    const content = document.querySelector(event.target.hash);
-    if (!content) return;
-    toggle(content);
-  },
-  false
+    'click',
+    function (event) {
+        if (event.target.classList.contains('experience')) {
+            event.preventDefault();
+            const content = document.getElementById('experience-section');
+
+            swap(content);
+        } else if(event.target.classList.contains('toggle')){
+            event.preventDefault();
+            const content = document.querySelector(event.target.hash);
+            if (!content) return;
+            toggle(content);
+        } return;
+    },
+    false
 );
